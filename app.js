ab1b324af3dd1ebc0c5c7ce6ef8b6f604e642432
Robinson Batista <robinso1919@gmail.com>
2020-12-27 14:20:17 -0400

    crud completed

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
+const Joi = require('joi');
 const express = require('express');
-const _ = require('underscore');
-const EventEmitter = require('events');
-
-const Logger = require('./logger');
-
+const { indexOf } = require('underscore');
 const app = express();
-const logger = new Logger();
+
+app.use(express.json());
+
 
 const courses = [
     {
@@ -24,17 +23,64 @@ const courses = [
 
 
 app.get('/', (req, res, next) => {
+    res.send("Hello world");
+});
+
+app.get('/api/courses/', (req, res, next) => {
     res.json(courses);
 });
 
-logger.on('messageLogger', arg => {
-    console.log('Listener called', arg)
-})
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCouse(req.body);    
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    
+    res.status(200).send(course);
+});
+
+app.put('/api/courses/:id', (req, res, next) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('Course not found');
+
+    const { error } = validateCouse(req.body);
+    
+    if(error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.status(400).send(course);
+});
+
+app.get('/api/courses/:id', (req, res, next) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('Course not found');
+    res.json(course);
+});
+
+app.delete('/api/courses/:id', (req, res, next) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('Course not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.status(200).send(course);
+});
 
 
-logger.log('message')
+const validateCouse = course => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
 
+    const result = schema.validate(course);
 
+    return result;
+}
 
 const port = process.env.PORT || 3000; 
 
--- logger.js
+++ logger.js
@@ -12,4 +12,33 @@ class Logger extends EventEmitter {
     }
 }
 
-module.exports = Logger;
+// module.exports = Logger;
+
+const http = require("https");
+
+const options = {
+	"method": "GET",
+	"hostname": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+	"port": null,
+	"path": "/apiservices/browsequotes/v1.0/US/USD/en-US/SDQ-sky/JFK-sky/2020-12-27?inboundpartialdate=2021-12-01",
+	"headers": {
+		"x-rapidapi-key": "cfb6401dbfmsh4cd960068254e59p15c792jsn024a69288566",
+		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+		"useQueryString": true
+	}
+};
+
+const req = http.request(options, function (res) {
+	const chunks = [];
+
+	res.on("data", function (chunk) {
+		chunks.push(chunk);
+	});
+
+	res.on("end", function () {
+		const body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
+});
+
+req.end();
